7647a6fb4d9c8ab7fa9d58b3b37e6377f2fdbeec
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2020-02-07 20:47:38 +0900

    Updat

--- init.el
+++ init.el
@@ -276,3 +276,15 @@
   (progn
     (global-set-key (kbd "C-c d") 'mc/mark-next-like-this)
     (global-set-key (kbd "C-c D") 'mc/mark-all-like-this)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((cider-shadow-cljs-default-options . "app")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
